420e510ce928458544d0a7223d6396a2ba32d710
aliaksei_vasilev <veterokpsb@gmail.com>
2023-02-10 09:04:51 +0300

    Update project add push jar file

--- AlekseiVasilyeuJava/src/main/java/test/Solution.java
+++ AlekseiVasilyeuJava/src/main/java/test/Solution.java
@@ -6,45 +6,60 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Задание 1: \n Введите число: ");
-        double numberEnter = scanner.nextDouble();
-        System.out.print("Задание 2: \n Введите имя: ");
-        String name = scanner.next();
-        System.out.print("Задание 3: \n Введите размер массива: ");
-        int arrayLength = scanner.nextInt();
-        scanner.close();
 
-        if (numberEnter == 7) {
-            System.out.println("Ответ 1: \n Привет");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Задание №1: \n Введите число: ");
+
+        if (scanner.hasNextDouble()) {
+            double numberEnter = scanner.nextDouble();
+            if (numberEnter == 7) {
+                System.out.println("Ответ №1: \n Числа равны.");
+            } else if (numberEnter > 7) {
+                System.out.println("Ответ №1: \n Привет.");
+            } else {
+                System.out.println("Ответ №1: \n Число меньше 7.");
+            }
         } else {
-            System.out.println("Ответ 1: \n Условие не выполнено");
+            System.out.println("Вы ввели не число");
         }
+
+        System.out.println("Задание №2: \n Введите имя: ");
+        scanner.nextLine();
+        String name = scanner.nextLine();
 
         if (name.equals("Вячеслав")) {
-            System.out.println("Ответ 2: \n Привет, Вячеслав");
+            System.out.println("Ответ №2: \n Привет, Вячеслав");
         } else {
-            System.out.println("Ответ 2: \n Нет такого имени");
+            System.out.println("Ответ №2: \n Нет такого имени");
         }
 
-        Random random = new Random();
-        int[] arrays = new int[arrayLength];
-        System.out.println("Ответ 3: \nМассив");
+        System.out.print("Задание №3: \n Введите размер массива: ");
 
-        for (int i = 0; i <= arrayLength - 1; i++) {
-            arrays[i] = random.nextInt(50);
-            System.out.print(arrays[i] + ". ");
-        }
+        if (scanner.hasNextInt()) {
+            int arrayLength = scanner.nextInt();
+            Random random = new Random();
+            int[] arrays = new int[arrayLength];
+            System.out.println("Ответ №3: \nМассив:");
 
-        System.out.println("\nЧисла из массива кратные 3: ");
-        for (int element : arrays) {
-            if (element % 3 == 0) {
-                System.out.print(element + ". ");
+            for (int i = 0; i <= arrayLength - 1; i++) {
+                arrays[i] = random.nextInt(50);
+                System.out.print(arrays[i] + ". ");
             }
+
+            System.out.println("\nЧисла из массива кратные 3: ");
+            for (int element : arrays) {
+                if (element % 3 == 0) {
+                    System.out.print(element + ". ");
+                }
+            }
+        } else {
+            System.out.println("Введите целое число");
         }
+        scanner.close();
         /* Ответ на 4 задание:
              [((())()(()))]
              Убрать 1 "]" закрывающую и поставить ")" закрывающую в конце
          */
     }
+
 }
